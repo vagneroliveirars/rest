890121cb0834dfa87c2583305af33760dc553386
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-10-19 20:20:05 -0200

    REST client with basic http authentication

---
+++ cap-08-ssl/src/main/java/br/com/geladaonline/client/Client.java
@@ -0,0 +1,42 @@
+package br.com.geladaonline.client;
+
+import javax.net.ssl.SSLContext;
+import javax.ws.rs.client.ClientBuilder;
+import javax.ws.rs.core.MediaType;
+
+import org.glassfish.jersey.SslConfigurator;
+import org.glassfish.jersey.client.authentication.HttpAuthenticationFeature;
+import org.glassfish.jersey.jettison.JettisonFeature;
+
+import br.com.geladaonline.model.rest.Cervejas;
+
+public class Client {
+
+	public static void main(String[] args) {
+		String baseDir = "/home/vagner/git/rest/cap-08-ssl/";
+		
+		SslConfigurator sslConfigurator = SslConfigurator
+				.newInstance()
+				.trustStoreFile(baseDir + "src/main/config/server.keystore")
+				.trustStorePassword("cervejaria");
+		
+		SSLContext context = sslConfigurator.createSSLContext();
+		
+		javax.ws.rs.client.Client client = ClientBuilder.newBuilder()
+				.sslContext(context)
+				.build();
+		
+		HttpAuthenticationFeature auth = HttpAuthenticationFeature.basic("admin", "123");
+		
+		client.register(auth).register(JettisonFeature.class);
+		
+		Cervejas cervejas = client
+				.target("https://localhost:8443/cervejaria/services")
+				.path("/cervejas")
+				.request(MediaType.APPLICATION_JSON)
+				.get(Cervejas.class);
+						
+		System.out.println(cervejas);
+	}
+
+}
