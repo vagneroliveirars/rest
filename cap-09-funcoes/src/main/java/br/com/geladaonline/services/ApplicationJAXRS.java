9171dbccc66159f8b596136402de0ba980393b94
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-10-28 15:29:52 -0200

    CPF validation.

---
+++ cap-09-funcoes/src/main/java/br/com/geladaonline/services/ApplicationJAXRS.java
@@ -0,0 +1,34 @@
+package br.com.geladaonline.services;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import javax.ws.rs.core.Application;
+
+import org.glassfish.jersey.jettison.JettisonFeature;
+
+/**
+ * This class sets up the JAX-RS application
+ *  
+ * @author vagner
+ *
+ */
+public class ApplicationJAXRS extends Application {
+	
+	@Override
+	public Map<String, Object> getProperties() {
+		Map<String, Object> properties = new HashMap<String, Object>();
+		properties.put("jersey.config.server.provider.packages", "br.com.geladaonline.services");
+		return properties;
+	}
+	
+	@Override
+	public Set<Object> getSingletons() {
+		Set<Object> singletons = new HashSet<Object>();
+		singletons.add(new JettisonFeature());
+		return singletons;
+	}
+	
+}
