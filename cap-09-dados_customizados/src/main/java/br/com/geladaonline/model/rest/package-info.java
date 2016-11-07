b0952eb26e8578a60e516eacbd5d8a29d2ad4027
vagneroliveirars <vagner.oliveirars@gmail.com>
2016-11-07 13:40:16 -0200

    Custom parsers to consume and produce images.

---
+++ cap-09-dados_customizados/src/main/java/br/com/geladaonline/model/rest/package-info.java
@@ -0,0 +1,5 @@
+@XmlJavaTypeAdapter(value=JaxbAdapter.class)
+package br.com.geladaonline.model.rest;
+
+import javax.ws.rs.core.Link.JaxbAdapter;
+import javax.xml.bind.annotation.adapters.XmlJavaTypeAdapter;
---
+++ cap-09-dados_customizados/src/main/java/br/com/geladaonline/services/ImageConsumer.java
@@ -0,0 +1,55 @@
+package br.com.geladaonline.services;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Type;
+
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response.Status;
+import javax.ws.rs.ext.MessageBodyReader;
+import javax.ws.rs.ext.Provider;
+
+import org.apache.commons.io.IOUtils;
+
+import br.com.geladaonline.model.Imagem;
+
+/**
+ * Custom parser to consume images
+ * 
+ * @author vagner
+ *
+ */
+@Provider
+public class ImageConsumer implements MessageBodyReader<Imagem> {
+
+	@Override
+	public boolean isReadable(Class<?> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType) {
+
+		return type.equals(Imagem.class) && mediaType.getType().equals("image");
+	}
+
+	@Override
+	public Imagem readFrom(Class<Imagem> type, Type genericType,
+			Annotation[] annotations, MediaType mediaType,
+			MultivaluedMap<String, String> httpHeaders, InputStream entityStream)
+			throws IOException, WebApplicationException {
+
+		if (!Imagem.EXTENSOES.containsKey(mediaType.toString())) {
+			System.out.println("Media type: " + mediaType.toString());
+			throw new WebApplicationException(Status.UNSUPPORTED_MEDIA_TYPE);
+		}
+		
+		String nome = httpHeaders.getFirst("nome");
+				
+		byte[] dados = IOUtils.toByteArray(entityStream);
+		
+		Imagem imagem = new Imagem(dados, nome, mediaType);
+		
+		return imagem;
+	}
+
+}
